7590e2ab3e8110e26fd6ae1cfe816eefdf9649a1
willywin99 <willywinsianturi@gmail.com>
2021-01-24 02:26:30 +0700

    Membuat Fungsi Login pada Framework Lumen

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\User;
 
+use Illuminate\Support\Str;
+
 class AuthController extends Controller
 {
     public function register(Request $request)
@@ -38,6 +40,35 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        # code...
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email', $email)->first();
+
+        if(Hash::check($password, $user->password)) {
+            $apiToken = base64_encode(Str::random(40));
+
+            $user->update([
+                'api_token' => $apiToken
+            ]);
+
+            return response()->json([
+                'success' => true,
+                'message' => "Login berhasil dilakukan",
+                'data' => [
+                    'user' => $user,
+                    'api_token' => $apiToken
+                ]
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "Login Fail!",
+                'data' => [
+                    'user' => $user,
+                    'api_token' => ''
+                ]
+            ], );
+        }
     }
 }
